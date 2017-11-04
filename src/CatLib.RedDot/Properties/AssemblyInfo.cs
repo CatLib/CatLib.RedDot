8fa5b27e09fc4efe237a1e385f8a3b479285f6d1
喵喵大人 <support@catlib.io>
2017-11-04 14:53:06 +0800

    first inited

---
+++ src/CatLib.RedDot/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CatLib.RedDot")]
+[assembly: AssemblyDescription("CatLib Red Dot Components")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CatLib")]
+[assembly: AssemblyProduct("CatLib.RedDot")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("CatLib")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("58769f72-17c9-40a7-93b2-c25d54e997d4")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
